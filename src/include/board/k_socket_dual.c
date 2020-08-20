5381346484fc2fa7f1f167c6e61286f00063de9c
krissgpl <44640016+krissgpl@users.noreply.github.com>
2020-08-20 19:15:30 +0200

    dodanie flagi channel state
    dwigniecie wersji i protokolu
    dostosowanie kodu

--- src/include/board/k_socket_dual.c
+++ src/include/board/k_socket_dual.c
@@ -67,7 +67,7 @@ void supla_esp_board_gpio_init(void) {
 
 }
 
-void supla_esp_board_set_channels(TDS_SuplaDeviceChannel_B *channels, unsigned char *channel_count) {
+void supla_esp_board_set_channels(TDS_SuplaDeviceChannel_C *channels, unsigned char *channel_count) {
 	
 
 	#ifdef __BOARD_k_socket_dual_ds18b20
@@ -84,21 +84,23 @@ void supla_esp_board_set_channels(TDS_SuplaDeviceChannel_B *channels, unsigned c
 
 	channels[0].Number = 0;
 	channels[0].Type = SUPLA_CHANNELTYPE_RELAY;
-	channels[0].FuncList = SUPLA_BIT_RELAYFUNC_POWERSWITCH \
-								| SUPLA_BIT_RELAYFUNC_LIGHTSWITCH;
+	channels[0].FuncList = SUPLA_BIT_FUNC_POWERSWITCH \
+								| SUPLA_BIT_FUNC_LIGHTSWITCH;
+	channels[0].Flags = SUPLA_CHANNEL_FLAG_CHANNELSTATE;
 	channels[0].Default = SUPLA_CHANNELFNC_POWERSWITCH;
 	channels[0].value[0] = supla_esp_gpio_relay_on(B_RELAY1_PORT);
 
 	channels[1].Number = 1;
 	channels[1].Type = SUPLA_CHANNELTYPE_RELAY;
-	channels[1].FuncList = SUPLA_BIT_RELAYFUNC_POWERSWITCH \
-								| SUPLA_BIT_RELAYFUNC_LIGHTSWITCH;
+	channels[1].FuncList = SUPLA_BIT_FUNC_POWERSWITCH \
+								| SUPLA_BIT_FUNC_LIGHTSWITCH;
+	channels[0].Flags = SUPLA_CHANNEL_FLAG_CHANNELSTATE;
 	channels[1].Default = SUPLA_CHANNELFNC_POWERSWITCH;
 	channels[1].value[0] = supla_esp_gpio_relay_on(B_RELAY2_PORT);
 	
 	channels[2].Number = 2;
 	channels[2].Type = SUPLA_CHANNELTYPE_RELAY;
-	channels[2].FuncList = SUPLA_BIT_RELAYFUNC_POWERSWITCH;
+	channels[2].FuncList = SUPLA_BIT_FUNC_POWERSWITCH;
 	channels[2].Default = 0;
 	channels[2].value[0] = supla_esp_gpio_relay_on(20);
 
