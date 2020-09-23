037e6f0e3b8d3f517e81bff91b968e47c96957ff
krissgpl <44640016+krissgpl@users.noreply.github.com>
2020-09-23 06:19:05 +0200

    Update k_socket.c
    dodanie zmiany stanu kanalu

--- src/include/board/k_socket.c
+++ src/include/board/k_socket.c
@@ -349,9 +349,10 @@ void supla_esp_board_gpiooutput_set_hi(uint8 port, uint8 hi) {
 		if ( supla_esp_cfg.FirmwareUpdate == 1 ) {
 			
 			//supla_esp_cfg_save(&supla_esp_cfg);
-			supla_esp_channel_value_changed(1, 1);
+			supla_esp_state.Relay[1] = 1;
 			supla_log(LOG_DEBUG, "value_changed 1 1 upd1");
 			supla_esp_save_state(SAVE_STATE_DELAY);
+			supla_esp_channel_value_changed(1, 1);
 			supla_log(LOG_DEBUG, "update restart ");
 			//os_delay_us(500000);
 			//supla_log(LOG_DEBUG, "update init restart ");
