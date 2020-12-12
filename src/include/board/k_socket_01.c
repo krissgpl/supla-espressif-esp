3fbdbed414b57030a5f388ae7971deb540b44eee
krissgpl <44640016+krissgpl@users.noreply.github.com>
2020-12-12 21:23:12 +0100

    dodanie bme280 init

--- src/include/board/k_socket_01.c
+++ src/include/board/k_socket_01.c
@@ -50,7 +50,12 @@ void supla_esp_board_gpio_init(void) {
 	PIN_PULLUP_EN(PERIPHS_IO_MUX_GPIO0_U);	// pullup gpio 0
 	
 	i2c_master_gpio_init();
-
+	
+	if(BME280_Init(BME280_OS_T_16, BME280_OS_P_16, BME280_OS_H_16,
+					BME280_FILTER_16, BME280_MODE_NORMAL, BME280_TSB_05))
+			supla_log(LOG_DEBUG, "BME280 init error");
+	else
+			supla_log(LOG_DEBUG, "BME280 init done");
 }
 
 void supla_esp_board_set_channels(TDS_SuplaDeviceChannel_C *channels, unsigned char *channel_count) {	
