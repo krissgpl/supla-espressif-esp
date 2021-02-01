e3dcf72a16d2515a44decb343cfc836ee49ec541
krissgpl <44640016+krissgpl@users.noreply.github.com>
2021-02-01 21:09:43 +0100

    test 1

--- src/include/board/k_switch_dual.c
+++ src/include/board/k_switch_dual.c
@@ -391,13 +391,13 @@ void ICACHE_FLASH_ATTR supla_esp_board_gpiooutput_set_hi(uint8 port, uint8 hi) {
 		
 		GPIO_OUTPUT_SET(GPIO_ID_PIN(port), hi == 1 ? 1 : 0);
 		
-		if (supla_last_state == STATE_CONNECTED) {
+		//if (supla_last_state == STATE_CONNECTED) {
 		
-			os_timer_disarm(&supla_gpio_timer1);
-			os_timer_disarm(&supla_gpio_timer2);
+			//os_timer_disarm(&supla_gpio_timer1);
+			//os_timer_disarm(&supla_gpio_timer2);
 		
 			supla_esp_gpio_set_hi(LED_RED_PORT, hi);
-		};
+		//};
 		
 		} else {
 		GPIO_OUTPUT_SET(GPIO_ID_PIN(port), hi == 1 ? 1 : 0);
@@ -407,8 +407,8 @@ void ICACHE_FLASH_ATTR supla_esp_board_gpiooutput_set_hi(uint8 port, uint8 hi) {
 		
 		GPIO_OUTPUT_SET(GPIO_ID_PIN(port), hi == 1 ? 1 : 0);
 		
-		os_timer_disarm(&supla_gpio_timer1);
-		os_timer_disarm(&supla_gpio_timer2);
+		//os_timer_disarm(&supla_gpio_timer1);
+		//os_timer_disarm(&supla_gpio_timer2);
 		
 		supla_esp_gpio_set_hi(LED_BLUE_PORT, hi);
 		
