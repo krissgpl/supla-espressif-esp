43423df3f8d29f934292873c7d60c73ce8db13d9
krissgpl <44640016+krissgpl@users.noreply.github.com>
2021-02-07 13:04:46 +0100

    test 3

--- src/include/board/k_switch_dual.h
+++ src/include/board/k_switch_dual.h
@@ -49,8 +49,8 @@
 #define B_UPD_PORT		 20
 
 #define BOARD_GPIO_OUTPUT_SET_HI if (supla_last_state == STATE_CONNECTED) \
-	{if (port == LED_RED_PORT) {supla_esp_gpio_set_led(supla_esp_gpio_output_is_hi(B_RELAY1_PORT), 1, 1); \
-	supla_log(LOG_DEBUG, "supla_esp_board_gpiooutput_set_hi %i", port); \
+	{if (port == LED_RED_PORT) {hi =!supla_esp_gpio_output_is_hi(B_RELAY1_PORT);	\
+	} else if (port == B_RELAY1_PORT) {supla_esp_gpio_set_led(hi, 1, 1); \
 	} else if (port == 20) {supla_esp_board_gpiooutput_set_hi(port, hi); \
 	return; } };
 				
