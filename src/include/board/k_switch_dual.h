8c2ace97b23d9c8cc7e636ccbcad15ecbe7a0c84
krissgpl <44640016+krissgpl@users.noreply.github.com>
2021-02-09 20:48:59 +0100

    fix 8

--- src/include/board/k_switch_dual.h
+++ src/include/board/k_switch_dual.h
@@ -59,10 +59,10 @@
 		if (port == LED_RED_PORT) {hi1 = supla_esp_gpio_output_is_hi(B_RELAY1_PORT);	\
 								   hi2 = supla_esp_gpio_output_is_hi(B_RELAY2_PORT);	\
 		} else if (port == B_RELAY1_PORT) {supla_esp_gpio_set_led(hi1, hi2, 1); \
-		} else if (port == B_RELAY2_PORT) {supla_esp_gpio_set_led(hi1, hi2, 1); }	\
-	} else if (port == 20) {supla_esp_board_gpiooutput_set_hi(port, hi); 	\
-							supla_log(LOG_DEBUG, "PORT 20 MAKRO");	\
-							return;  };	\
+		} else if (port == B_RELAY2_PORT) {supla_esp_gpio_set_led(hi1, hi2, 1); }; }	\
+	if (port == 20) {supla_esp_board_gpiooutput_set_hi(port, hi); 	\
+					supla_log(LOG_DEBUG, "PORT 20 MAKRO");	\
+					return;  };	\
 }
 				
 void ICACHE_FLASH_ATTR supla_esp_board_gpiooutput_set_hi(int port, char hi);
