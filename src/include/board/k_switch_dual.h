cd0cc6de5594633704560bd0a54203216314facb
krissgpl <kris.sg@wp.pl>
2021-09-29 07:19:10 +0200

    dodatkowy log dla timera

--- src/include/board/k_switch_dual.h
+++ src/include/board/k_switch_dual.h
@@ -101,6 +101,8 @@
 				
 #define BOARD_ON_CHANNEL_STATE_PREPARE	state->Fields |= SUPLA_CHANNELSTATE_FIELD_LASTCONNECTIONRESETCAUSE;	\
 										state->LastConnectionResetCause = supla_esp_cfg.UpdateStatus;
+										
+#define BOARD_ON_COUNTDOWN_START	supla_log(LOG_DEBUG, "BOARD_ON_COUNTDOWN_START");
 				
 void ICACHE_FLASH_ATTR supla_esp_board_gpiooutput_set_hi(int port, char hi);
 
