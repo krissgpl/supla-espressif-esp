673c2ea491b5767d7a3a92b0f9e3641dedd88f86
krissgpl <kris.sg@wp.pl>
2021-10-17 12:19:34 +0200

    przeniesienie timera do voida

--- src/include/board/k_gate_module_v3.c
+++ src/include/board/k_gate_module_v3.c
@@ -400,6 +400,7 @@ void supla_gate_light_ON() {
 		
 			Licznik = 0;
 			supla_esp_devconn_send_action_trigger(4, SUPLA_ACTION_CAP_TOGGLE_x1);
+			supla_esp_gpio_relay_set_duration_timer(TMR_channel, hi, supla_esp_state.Time2Left[TMR_channel], 0);
 		};
 	};
 }
@@ -471,7 +472,6 @@ if ( port == 22 ) {
 		supla_esp_channel_value_changed(TMR_channel, hi);
 		
 		if ( hi==1 ) { supla_log(LOG_DEBUG, "wlaczenie oswietlenia bramy"); 
-						supla_esp_gpio_relay_set_duration_timer(TMR_channel, hi, supla_esp_state.Time2Left[TMR_channel], 0);
 						supla_gate_light_ON();	};
 		if ( hi==0 ) { supla_log(LOG_DEBUG, "wylaczenie oswietlenia bramy"); 
 						supla_gate_light_OFF();	};
