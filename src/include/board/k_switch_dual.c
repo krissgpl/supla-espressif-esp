76bb82753e98f0b23e3fa7d6c5231145ef73080d
krissgpl <kris.sg@wp.pl>
2021-11-18 17:34:51 +0100

    Update k_switch_dual.c

--- src/include/board/k_switch_dual.c
+++ src/include/board/k_switch_dual.c
@@ -177,10 +177,9 @@ void supla_esp_board_set_channels(TDS_SuplaDeviceChannel_C *channels, unsigned c
 		}
 
 	channels[0].Number = 0;
-	channels[0].Type = SUPLA_CHANNELTYPE_ACTIONTRIGGER;
+	channels[0].Type = SUPLA_CHANNELTYPE_RELAY;
 	channels[0].FuncList = SUPLA_BIT_FUNC_POWERSWITCH \
 								| SUPLA_BIT_FUNC_LIGHTSWITCH;
-							//	| SUPLA_CHANNELFNC_ACTIONTRIGGER;
 	channels[0].Flags = SUPLA_CHANNEL_FLAG_CHANNELSTATE;							
 	channels[0].Default = SUPLA_BIT_FUNC_LIGHTSWITCH;
 	channels[0].value[0] = supla_esp_gpio_relay_on(B_RELAY1_PORT);
