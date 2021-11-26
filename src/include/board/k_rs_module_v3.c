20a248fa993853c7ffda26fe7bfee9e59c76aee3
Kris_work <krisu.sg@gmail.com>
2021-11-26 15:51:39 +0100

    dostosowanie kodu do nowego gita
    dzwigniecie wersji

--- src/include/board/k_rs_module_v3.c
+++ src/include/board/k_rs_module_v3.c
@@ -76,12 +76,12 @@ void supla_esp_baord_Led_OFF_cb(void *timer_arg) {
 
 void ICACHE_FLASH_ATTR supla_esp_board_gpio_init(void) {
 				
-	supla_input_cfg[0].type = INPUT_TYPE_BTN_MONOSTABLE_RS;
+	supla_input_cfg[0].type = INPUT_TYPE_BTN_MONOSTABLE;
 	supla_input_cfg[0].gpio_id = B_BTN1_PORT;
 	supla_input_cfg[0].flags = INPUT_FLAG_PULLUP | INPUT_FLAG_CFG_BTN;
     supla_input_cfg[0].relay_gpio_id = B_RELAY1_PORT;
 
-	supla_input_cfg[1].type = INPUT_TYPE_BTN_MONOSTABLE_RS;
+	supla_input_cfg[1].type = INPUT_TYPE_BTN_MONOSTABLE;
 	supla_input_cfg[1].gpio_id = B_BTN2_PORT;
 	supla_input_cfg[1].flags = INPUT_FLAG_PULLUP | INPUT_FLAG_CFG_BTN;
 	supla_input_cfg[1].relay_gpio_id = B_RELAY2_PORT;
@@ -358,7 +358,7 @@ void ICACHE_FLASH_ATTR supla_esp_board_gpio_on_input_active(void* _input_cfg) {
 
     supla_input_cfg_t* input_cfg = (supla_input_cfg_t*)_input_cfg;
 
-    if (input_cfg->type == INPUT_TYPE_BTN_MONOSTABLE_RS) {
+    if (input_cfg->type == INPUT_TYPE_BTN_MONOSTABLE) {
 
     supla_log(LOG_DEBUG, "RELAY HI RS");
 	
@@ -388,7 +388,7 @@ supla_esp_board_gpio_on_input_inactive(void* _input_cfg) {
 
     supla_input_cfg_t* input_cfg = (supla_input_cfg_t*)_input_cfg;
 
-    if (input_cfg->type == INPUT_TYPE_BTN_MONOSTABLE_RS) {
+    if (input_cfg->type == INPUT_TYPE_BTN_MONOSTABLE) {
 
 		supla_log(LOG_DEBUG, "RELAY LO RS");
 	
