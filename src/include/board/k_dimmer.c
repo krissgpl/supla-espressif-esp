8cb1cb249152e08634422679341a34768f5fb022
Kris_work <krisu.sg@gmail.com>
2021-12-17 16:57:30 +0100

    wyl smoothly

--- src/include/board/k_dimmer.c
+++ src/include/board/k_dimmer.c
@@ -91,10 +91,9 @@ void ICACHE_FLASH_ATTR supla_esp_board_set_channels(TDS_SuplaDeviceChannel_C *ch
 	channels[1].value[0] = supla_esp_gpio_relay_on(B_RELAY1_PORT);
 }
 
-char ICACHE_FLASH_ATTR supla_esp_board_set_rgbw_value(int ChannelNumber, int *Color, float *ColorBrightness, float *Brightness, char smoothly) {
+char ICACHE_FLASH_ATTR supla_esp_board_set_rgbw_value(int ChannelNumber, int *Color, float *ColorBrightness, float *Brightness ) {
 
 	dimmer_brightness = *Brightness;
-	smoothly = 1;
 	
 	int hi;
 	hi = supla_esp_gpio_output_is_hi(B_RELAY1_PORT);
