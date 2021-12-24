bc5ba890f2213abcb6f2fb206f5701ec7553e0ba
krissgpl <kris.sg@wp.pl>
2021-12-24 15:23:04 +0100

    brakujaca deklaracja ?

--- src/include/board/k_dimmer.h
+++ src/include/board/k_dimmer.h
@@ -67,7 +67,8 @@
 								supla_log(LOG_DEBUG, "CHANNEL active = %i", input_cfg->channel);	\
 								supla_dimmer_smooth(0); };
 							  
-#define BOARD_INTR_HANDLER	if ( input_cfg->gpio_id == 12 || input_cfg->gpio_id  == 14 ) {	\
+#define BOARD_INTR_HANDLER	input_cfg = &supla_input_cfg[a];	\
+							if ( input_cfg->gpio_id == 12 || input_cfg->gpio_id  == 14 ) {	\
 							supla_log(LOG_DEBUG, "Input intr test");	\
 							supla_dimmer_smooth(2); };
 
