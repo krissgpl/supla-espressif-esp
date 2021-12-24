027d602ce53f1479774324910b16933f439afc3f
krissgpl <kris.sg@wp.pl>
2021-12-24 15:36:51 +0100

    dodatkowe log

--- src/include/board/k_dimmer.c
+++ src/include/board/k_dimmer.c
@@ -135,9 +135,9 @@ void work_timer_cb(void *timer_arg) {
 	
 	supla_log(LOG_DEBUG, "Set dimmer 0");
 	Licznik = supla_esp_state.brightness[0];
-	os_timer_disarm(&dimmer_timer);
-	os_timer_setfn(&dimmer_timer, (os_timer_func_t *)dimmer_timer_OFF_cb, NULL);
-	os_timer_arm(&dimmer_timer, 20, 1);
+	//os_timer_disarm(&dimmer_timer);
+	//os_timer_setfn(&dimmer_timer, (os_timer_func_t *)dimmer_timer_OFF_cb, NULL);
+	//os_timer_arm(&dimmer_timer, 20, 1);
 }
 
 void supla_dimmer_smooth(int hi) {
@@ -150,9 +150,9 @@ if ( supla_esp_gpio_output_is_hi(B_BLOKADA) == 0 ) {
 	
 		if( hi == 1 ) { supla_log(LOG_DEBUG, "Set dimmer 1");
 					Licznik = 0;
-					os_timer_disarm(&dimmer_timer);
-					os_timer_setfn(&dimmer_timer, (os_timer_func_t *)dimmer_timer_ON_cb, NULL);
-					os_timer_arm(&dimmer_timer, 20, 1);
+					//os_timer_disarm(&dimmer_timer);
+					//os_timer_setfn(&dimmer_timer, (os_timer_func_t *)dimmer_timer_ON_cb, NULL);
+					//os_timer_arm(&dimmer_timer, 20, 1);
 					//os_timer_disarm(&work_timer);
 					//os_timer_setfn(&work_timer, (os_timer_func_t *)work_timer_cb, NULL);
 					//os_timer_arm(&work_timer, 30000, 0);
@@ -163,9 +163,9 @@ if ( supla_esp_gpio_output_is_hi(B_BLOKADA) == 0 ) {
 	if ( hi == 2 || Start == 0 ) { supla_log(LOG_DEBUG, "Set dimmer 1");
 					Licznik = 0;
 					Start = 1;
-					os_timer_disarm(&dimmer_timer);
-					os_timer_setfn(&dimmer_timer, (os_timer_func_t *)dimmer_timer_ON_cb, NULL);
-					os_timer_arm(&dimmer_timer, 20, 1);
+					//os_timer_disarm(&dimmer_timer);
+					//os_timer_setfn(&dimmer_timer, (os_timer_func_t *)dimmer_timer_ON_cb, NULL);
+					//os_timer_arm(&dimmer_timer, 20, 1);
 					//os_timer_disarm(&work_timer);
 					//os_timer_setfn(&work_timer, (os_timer_func_t *)work_timer_cb, NULL);
 					//os_timer_arm(&work_timer, 30000, 0);
