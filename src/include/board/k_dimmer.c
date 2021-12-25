74d033586393105945f43b8e67dbd03e696ba68e
krissgpl <kris.sg@wp.pl>
2021-12-25 13:30:53 +0100

    final ota test

--- src/include/board/k_dimmer.c
+++ src/include/board/k_dimmer.c
@@ -135,7 +135,10 @@ void dimmer_timer_OFF_cb(void *timer_arg) {
 void work_timer_cb(void *timer_arg) {
 	
 	supla_log(LOG_DEBUG, "Set dimmer 0");
-	Licznik = supla_esp_state.brightness[0];
+	
+	if ( supla_esp_gpio_output_is_hi(B_HARMONOGRAM) == 1 ) { Licznik = 100;
+	} else { Licznik = supla_esp_state.brightness[0]; };
+	
 	Wlacznik = 0;
 	os_timer_disarm(&dimmer_timer);
 	os_timer_setfn(&dimmer_timer, (os_timer_func_t *)dimmer_timer_OFF_cb, NULL);
