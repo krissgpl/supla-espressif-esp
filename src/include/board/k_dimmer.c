1f311e3509ae33be362fc3575c0cb445947e2bed
krissgpl <kris.sg@wp.pl>
2022-01-09 11:21:42 +0100

    input dodatkowe logi

--- src/include/board/k_dimmer.c
+++ src/include/board/k_dimmer.c
@@ -147,7 +147,7 @@ void work_timer_cb(void *timer_arg) {
 
 void supla_dimmer_smooth(int hi) {
 
-if ( supla_esp_gpio_output_is_hi(B_SWITCH) == 1 && supla_esp_cfg.FirmwareUpdate == 0 ) {
+/*if ( supla_esp_gpio_output_is_hi(B_SWITCH) == 1 && supla_esp_cfg.FirmwareUpdate == 0 ) {
 	
 	Czas = supla_esp_state.brightness[1] * 1000;
 	Wlacznik = Wlacznik + 1;
@@ -174,7 +174,7 @@ if ( supla_esp_gpio_output_is_hi(B_SWITCH) == 1 && supla_esp_cfg.FirmwareUpdate 
 					os_timer_disarm(&work_timer);
 					os_timer_setfn(&work_timer, (os_timer_func_t *)work_timer_cb, NULL);
 					os_timer_arm(&work_timer, Czas, 0); };
-}; 
+}; */
 }
 
 void ICACHE_FLASH_ATTR supla_esp_board_pwm_init(void) {
--- src/include/board/k_dimmer.h
+++ src/include/board/k_dimmer.h
@@ -73,6 +73,8 @@
 												return supla_esp_state.Relay[8] == 1 ? 1 : 0;	};	\
 								
 #define BOARD_ON_INPUT_INACTIVE if (supla_last_state == STATE_CONNECTED) { \
+									supla_log(LOG_DEBUG, "CHANNEL inactive gpio 12, hi = %i", gpio__input_get(B_SENSOR_PORT1));	\
+									supla_log(LOG_DEBUG, "CHANNEL inactive gpio 14, hi = %i", gpio__input_get(B_SENSOR_PORT2));	\
 									if ( input_cfg->gpio_id == 12 && supla_esp_gpio_output_is_hi(B_SENSOR_BLOCK1) == 1 ) {	\
 										supla_log(LOG_DEBUG, "CHANNEL inactive = %i", input_cfg->channel);	\
 										supla_dimmer_smooth(1); };	\
@@ -81,6 +83,8 @@
 										supla_dimmer_smooth(1); }; }
 
 #define BOARD_ON_INPUT_ACTIVE if (supla_last_state == STATE_CONNECTED) { \
+								supla_log(LOG_DEBUG, "CHANNEL active gpio 12, hi = %i", gpio__input_get(B_SENSOR_PORT1));	\
+								supla_log(LOG_DEBUG, "CHANNEL active gpio 14, hi = %i", gpio__input_get(B_SENSOR_PORT2));	\
 								if ( input_cfg->gpio_id == 12 && supla_esp_gpio_output_is_hi(B_SENSOR_BLOCK1) == 1 ) {	\
 									supla_log(LOG_DEBUG, "CHANNEL active = %i", input_cfg->channel);	\
 									supla_dimmer_smooth(0); };	\
