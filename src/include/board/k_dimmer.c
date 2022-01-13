509c751269008122c81abf4a999eaaa0a2a33187
Kris_work <krisu.sg@gmail.com>
2022-01-13 17:36:49 +0100

    dodatkowe logi

--- src/include/board/k_dimmer.c
+++ src/include/board/k_dimmer.c
@@ -150,6 +150,8 @@ void work_timer_cb(void *timer_arg) {
 void supla_dimmer_smooth(int in1, int in2) {
 	
 	supla_log(LOG_DEBUG, "dimmer_smooth CH1 = %i, CH2 = %i", in1, in2);
+	supla_log(LOG_DEBUG, "dimmer_smooth B_SWITCH = %i", supla_esp_gpio_output_is_hi(B_SWITCH));
+	supla_log(LOG_DEBUG, "dimmer_smooth FirmwareUpdate = %i", supla_esp_cfg.FirmwareUpdate);
 	
 	if ( supla_esp_gpio_output_is_hi(B_SWITCH) == 1 && supla_esp_cfg.FirmwareUpdate == 0 ) {
 		
