823192936d55bb0675570af8778893ec51daa764
Kris_work <krisu.sg@gmail.com>
2022-01-17 17:47:06 +0100

    dodanie kompilacji user2

--- src/compilefw.sh
+++ src/compilefw.sh
@@ -4,8 +4,8 @@ HEIGHT=15
 WIDTH=40
 CHOICE_HEIGHT=4
 BACKTITLE="SUPLA FIRMWARE COMPILER"
-TITLE="PŁYTKI"
-MENU="Wybierz płytkę:"
+TITLE="PLYTKI"
+MENU="Wybierz plytke:"
 
 OPTIONS=(1 "k_rs_module_v3"
          2 "k_dimmer"
@@ -35,3 +35,15 @@ esac
 ./build.sh "$BOARD"
 
 echo "gotowe"
+
+dialog --clear --title "Czy skompilowac USER2 Tak/nie" --backtitle "USER2" --yesno "Twój wybór:" 10 40
+    YOUR_CHOOSE=$?;
+    if [ "$YOUR_CHOOSE" == 0 ];
+    then
+        ./build.sh "$BOARD" user2
+    elif [ "$YOUR_CHOOSE" == 1 ];
+    then
+        echo "Wybrałeś Nie";
+    else
+        echo "Niczego nie wybrałeś";
+    fi
