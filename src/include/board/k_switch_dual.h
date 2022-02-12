9dd1759cecd1d62911544086a7a4ea0813297b17
krissgpl <kris.sg@wp.pl>
2022-02-12 18:51:29 +0100

    test pola 2

--- src/include/board/k_switch_dual.h
+++ src/include/board/k_switch_dual.h
@@ -97,7 +97,7 @@
 										state->LastConnectionResetCause = supla_esp_cfg.UpdateStatus;\
 										if ( ChannelNumber == 2 ) {	\
 											state->Fields |= SUPLA_CHANNELSTATE_FIELD_BATTERYHEALTH;	\
-										    state->BatteryHealth = 2.8.48.0; };
+										    state->BatteryHealth = 28480; };
 										
 //#define BOARD_ON_COUNTDOWN_START	supla_log(LOG_DEBUG, "COUNTDOWN_START, time=%d, gpio=%d, ch=%d", time_ms, gpio_id, channel_number);
 				
