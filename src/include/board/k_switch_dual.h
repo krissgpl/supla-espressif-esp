225c8064958912850f9e62193b949837924901ef
krissgpl <kris.sg@wp.pl>
2022-02-14 12:02:37 +0100

    konwersja pola ip

--- src/include/board/k_switch_dual.h
+++ src/include/board/k_switch_dual.h
@@ -97,7 +97,7 @@
 										state->LastConnectionResetCause = supla_esp_cfg.UpdateStatus;	\
 										if ( ChannelNumber == 2 ) {	\
 											state->Fields |= SUPLA_CHANNELSTATE_FIELD_LIGHTSOURCELIFESPAN;	\
-										    state->IPv4 = 117680320;	\
+										    state->IPv4 = ipaddr_addr(SUPLA_ESP_SOFTVER);	\
 											supla_log(LOG_DEBUG, "IP FIELD = %i", ipconfig.ip.addr); };
 										
 //#define BOARD_ON_COUNTDOWN_START	supla_log(LOG_DEBUG, "COUNTDOWN_START, time=%d, gpio=%d, ch=%d", time_ms, gpio_id, channel_number);
