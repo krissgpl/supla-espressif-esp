2b895a31815455867981368eb3c54fc7883314d2
Kris_work <krisu.sg@gmail.com>
2022-02-21 17:40:22 +0100

    dodatkowe logi dla input

--- src/user/supla_esp_gpio.c
+++ src/user/supla_esp_gpio.c
@@ -1178,6 +1178,7 @@ supla_esp_gpio_on_input_active(supla_input_cfg_t *input_cfg) {
       input_cfg->type == INPUT_TYPE_BTN_BISTABLE ||
       advanced_mode) &&
       input_cfg->relay_gpio_id != 255) {
+	supla_log(LOG_DEBUG, "INPUT_FLAG_TRIGGER_ON_PRESS active");
     supla_roller_shutter_cfg_t *rs_cfg =
       supla_esp_gpio_get_rs__cfg(input_cfg->relay_gpio_id);
     if (rs_cfg != NULL) {
@@ -1219,6 +1220,7 @@ supla_esp_gpio_on_input_inactive(supla_input_cfg_t *input_cfg) {
         !(input_cfg->flags & INPUT_FLAG_TRIGGER_ON_PRESS)) ||
       input_cfg->type == INPUT_TYPE_BTN_BISTABLE) &&
       input_cfg->relay_gpio_id != 255) {
+	supla_log(LOG_DEBUG, "INPUT_FLAG_TRIGGER_ON_PRESS inactive");
     supla_roller_shutter_cfg_t *rs_cfg =
       supla_esp_gpio_get_rs__cfg(input_cfg->relay_gpio_id);
     if (rs_cfg != NULL) {
