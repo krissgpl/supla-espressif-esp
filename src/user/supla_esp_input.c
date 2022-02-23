1b209d7718ed644dab3d3419456b8b0f9448b384
Kris_work <krisu.sg@gmail.com>
2022-02-23 17:21:11 +0100

    dodatkowy log at

--- src/user/supla_esp_input.c
+++ src/user/supla_esp_input.c
@@ -569,6 +569,7 @@ supla_esp_input_send_action_trigger(supla_input_cfg_t *input_cfg, int action) {
          break;
        case 2:
          action = SUPLA_ACTION_CAP_SHORT_PRESS_x2;
+		 supla_log(LOG_DEBUG, "action = SUPLA_ACTION_CAP_SHORT_PRESS_x2;");
          break;
        case 3:
          action = SUPLA_ACTION_CAP_SHORT_PRESS_x3;
