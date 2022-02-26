d01074108f96ba3ce9aaa0eaa793d98688798f32
Kris_work <krisu.sg@gmail.com>
2022-02-26 12:08:04 +0100

    porzadki, test dwoch at

--- src/include/board/k_switch_dual.c
+++ src/include/board/k_switch_dual.c
@@ -110,8 +110,8 @@ void supla_esp_board_gpio_init(void) {
 	supla_input_cfg[0].type = INPUT_TYPE_BTN_MONOSTABLE;
 	supla_input_cfg[0].gpio_id = B_BTN1_PORT;
 	supla_input_cfg[0].flags = INPUT_FLAG_PULLUP | INPUT_FLAG_CFG_BTN | INPUT_FLAG_TRIGGER_ON_PRESS;
-	supla_input_cfg[0].action_trigger_cap = SUPLA_ACTION_CAP_SHORT_PRESS_x2;
-	supla_input_cfg[0].active_triggers = SUPLA_ACTION_CAP_SHORT_PRESS_x2;
+	supla_input_cfg[0].action_trigger_cap = SUPLA_ACTION_CAP_SHORT_PRESS_x2 | SUPLA_ACTION_CAP_SHORT_PRESS_x3;
+	supla_input_cfg[0].active_triggers = SUPLA_ACTION_CAP_SHORT_PRESS_x2 | SUPLA_ACTION_CAP_SHORT_PRESS_x3;
 	supla_input_cfg[0].relay_gpio_id = B_RELAY1_PORT;
 	supla_input_cfg[0].channel = 0;
 
@@ -165,7 +165,7 @@ void supla_esp_board_gpio_init(void) {
 		supla_log(LOG_DEBUG, "ustaw gpio10 high wl zasilania DH");
 	};
 	
-	supla_esp_input_set_active_triggers(supla_input_cfg, SUPLA_ACTION_CAP_SHORT_PRESS_x2);
+	supla_esp_input_set_active_triggers(supla_input_cfg, SUPLA_ACTION_CAP_SHORT_PRESS_x2 | SUPLA_ACTION_CAP_SHORT_PRESS_x3);
 	
 }
 
--- src/user/supla_esp_input.c
+++ src/user/supla_esp_input.c
@@ -134,7 +134,6 @@ void GPIO_ICACHE_FLASH supla_esp_input_notify_state_change(
 #endif
 
   if (supla_esp_input_is_advanced_mode_enabled(input_cfg)) {
-	supla_log(LOG_DEBUG, "advanced mode enabled !");			//	moje
     os_timer_setfn(&input_cfg->timer, supla_esp_input_advanced_timer_cb,
         input_cfg);
     supla_esp_input_advanced_state_change_handling(input_cfg, new_state);
@@ -446,7 +445,6 @@ void GPIO_ICACHE_FLASH supla_esp_input_advanced_state_change_handling(
         (input_cfg->type & INPUT_TYPE_BTN_BISTABLE)) {
 
       input_cfg->click_counter++;
-	  supla_log(LOG_DEBUG, "click_counter : %d", input_cfg->click_counter);	// moje
 
       if (input_cfg->type == INPUT_TYPE_BTN_BISTABLE) {
         if (new_state == INPUT_STATE_ACTIVE) {
@@ -538,7 +536,6 @@ void GPIO_ICACHE_FLASH supla_esp_input_advanced_timer_cb(void *timer_arg) {
 void GPIO_ICACHE_FLASH
 supla_esp_input_send_action_trigger(supla_input_cfg_t *input_cfg, int action) {
   if (action == 0) {
-	  supla_log(LOG_DEBUG, "send_action_trigger, action == 0");	// moje
     if (input_cfg->click_counter == -1) {
       return;
     }
@@ -571,10 +568,6 @@ supla_esp_input_send_action_trigger(supla_input_cfg_t *input_cfg, int action) {
          break;
        case 2:
          action = SUPLA_ACTION_CAP_SHORT_PRESS_x2;
-		 supla_log(LOG_DEBUG, "action = SUPLA_ACTION_CAP_SHORT_PRESS_x2;");	// moje
-		 #ifdef BOARD_SEND_AT
-			BOARD_SEND_AT;
-		 #endif
          break;
        case 3:
          action = SUPLA_ACTION_CAP_SHORT_PRESS_x3;
@@ -607,7 +600,7 @@ supla_esp_input_send_action_trigger(supla_input_cfg_t *input_cfg, int action) {
     }
 
   }
-
+  
   if (!(action & input_cfg->active_triggers)) {
     supla_log(LOG_DEBUG, "Input: action %d is not activated", action);
     return;
@@ -635,6 +628,11 @@ supla_esp_input_send_action_trigger(supla_input_cfg_t *input_cfg, int action) {
     supla_esp_mqtt_send_action_trigger(input_cfg->channel, action);
 #endif /*MQTT_HA_ACTION_TRIGGER_SUPPORT*/
 #endif /*MQTT_SUPPORT_ENABLED*/
+
+#ifdef BOARD_SEND_AT		// moje do lokalnego wykorzystania multiclicow
+	BOARD_SEND_AT;
+#endif
+
   }
 }
 
