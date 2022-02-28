7763a788e8d64a7f1cff4f0acb8538bdd515a3f4
krissgpl <kris.sg@wp.pl>
2022-02-28 17:31:45 +0100

    powinno byc

--- src/include/board/k_switch_dual.c
+++ src/include/board/k_switch_dual.c
@@ -190,7 +190,7 @@ void supla_esp_board_set_channels(TDS_SuplaDeviceChannel_C *channels, unsigned c
 	channels[0].Default = SUPLA_CHANNELFNC_LIGHTSWITCH;
 	channels[0].ActionTriggerCaps = supla_input_cfg[0].action_trigger_cap;
 	channels[0].actionTriggerProperties.disablesLocalOperation = SUPLA_ACTION_CAP_TOGGLE_x1 | SUPLA_ACTION_CAP_SHORT_PRESS_x1;
-	channels[0].actionTriggerProperties.relatedChannelNumber = 0;
+	channels[0].actionTriggerProperties.relatedChannelNumber = 1;
 	channels[0].value[0] = supla_esp_gpio_relay_on(B_RELAY1_PORT);
 	
 	channels[1].Number = 1;
