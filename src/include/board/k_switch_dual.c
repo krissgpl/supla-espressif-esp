77dd8cfa10338b2bc0e9fb64026d48654fe388aa
krissgpl <kris.sg@wp.pl>
2022-02-28 17:23:58 +0100

    dodatkowe ustawienia

--- src/include/board/k_switch_dual.c
+++ src/include/board/k_switch_dual.c
@@ -166,7 +166,7 @@ void supla_esp_board_gpio_init(void) {
 	};
 	
 	//supla_esp_input_set_active_triggers(&(supla_input_cfg[0]), SUPLA_ACTION_CAP_SHORT_PRESS_x2 | SUPLA_ACTION_CAP_SHORT_PRESS_x3);
-	supla_esp_input_set_active_triggers(&(supla_input_cfg[1]), SUPLA_ACTION_CAP_SHORT_PRESS_x2 | SUPLA_ACTION_CAP_SHORT_PRESS_x3);
+	//supla_esp_input_set_active_triggers(&(supla_input_cfg[1]), SUPLA_ACTION_CAP_SHORT_PRESS_x2 | SUPLA_ACTION_CAP_SHORT_PRESS_x3);
 	
 }
 
@@ -190,6 +190,7 @@ void supla_esp_board_set_channels(TDS_SuplaDeviceChannel_C *channels, unsigned c
 	channels[0].Default = SUPLA_CHANNELFNC_LIGHTSWITCH;
 	channels[0].ActionTriggerCaps = supla_input_cfg[0].action_trigger_cap;
 	channels[0].actionTriggerProperties.disablesLocalOperation = SUPLA_ACTION_CAP_TOGGLE_x1 | SUPLA_ACTION_CAP_SHORT_PRESS_x1;
+	channels[0].actionTriggerProperties.relatedChannelNumber = 0;
 	channels[0].value[0] = supla_esp_gpio_relay_on(B_RELAY1_PORT);
 	
 	channels[1].Number = 1;
