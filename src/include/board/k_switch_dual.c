abbe9e1907d68d13111076872811d67cf3f385b9
Kris_work <krisu.sg@gmail.com>
2022-03-05 11:43:36 +0100

    zmiana kanalu na at

--- src/include/board/k_switch_dual.c
+++ src/include/board/k_switch_dual.c
@@ -109,14 +109,14 @@ void supla_esp_board_gpio_init(void) {
 		
 	supla_input_cfg[0].type = INPUT_TYPE_BTN_MONOSTABLE;
 	supla_input_cfg[0].gpio_id = B_BTN1_PORT;
-	supla_input_cfg[0].flags = INPUT_FLAG_PULLUP | INPUT_FLAG_CFG_BTN | INPUT_FLAG_TRIGGER_ON_PRESS;
+	supla_input_cfg[0].flags = INPUT_FLAG_PULLUP | INPUT_FLAG_CFG_BTN;
 	supla_input_cfg[0].action_trigger_cap = SUPLA_ACTION_CAP_HOLD |
 											SUPLA_ACTION_CAP_SHORT_PRESS_x2 |
 											SUPLA_ACTION_CAP_SHORT_PRESS_x3 |
 											SUPLA_ACTION_CAP_SHORT_PRESS_x4 |
 											SUPLA_ACTION_CAP_SHORT_PRESS_x5;
 	supla_input_cfg[0].relay_gpio_id = B_RELAY1_PORT;
-	supla_input_cfg[0].channel = 0;
+	supla_input_cfg[0].channel = 2;
 
 	supla_input_cfg[1].type = INPUT_TYPE_BTN_MONOSTABLE;
 	supla_input_cfg[1].gpio_id = B_BTN2_PORT;
