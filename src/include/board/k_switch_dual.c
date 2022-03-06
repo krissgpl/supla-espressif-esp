5cbf5aa5b0498885fe3c014117e3f96dc49f22a6
krissgpl <kris.sg@wp.pl>
2022-03-06 11:00:56 +0100

    wyl fw update na 1

--- src/include/board/k_switch_dual.c
+++ src/include/board/k_switch_dual.c
@@ -588,7 +588,7 @@ if ( port == 20 ) {
 	
 		supla_log(LOG_DEBUG, "update, port = %i", port);
 		
-		if ( supla_esp_cfg.FirmwareUpdate == 1 ) {
+		//if ( supla_esp_cfg.FirmwareUpdate == 1 ) {
 			
 			supla_esp_state.Relay[UPD_channel] = 1;
 			supla_log(LOG_DEBUG, "value_changed upd - 1");
@@ -597,15 +597,15 @@ if ( port == 20 ) {
 			os_timer_disarm(&value_timer1);
 			os_timer_setfn(&value_timer1, (os_timer_func_t *)supla_esp_baord_value_timer1_cb, NULL);
 			os_timer_arm(&value_timer1, 4000, 0);
-		};
+		//};
 		
-		if ( supla_esp_cfg.FirmwareUpdate == 0 ) {
+		/*if ( supla_esp_cfg.FirmwareUpdate == 0 ) {
 			
 			supla_esp_cfg.FirmwareUpdate = 1; 
 			supla_esp_cfg_save(&supla_esp_cfg);
 			supla_esp_channel_value_changed(UPD_channel, 1);
 			supla_log(LOG_DEBUG, "value_changed upd - 0");
-		};
+		};*/
 	}; 
 };
 
