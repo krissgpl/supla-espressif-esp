a7a0a2e46257c172f44ce8dda4754d630d9d35bb
Kris_work <krisu.sg@gmail.com>
2022-03-23 17:47:54 +0100

    nowe funkcje
    dzwigniecie wersji

--- src/include/board/k_socket_SSR.c
+++ src/include/board/k_socket_SSR.c
@@ -253,7 +253,8 @@ char *ICACHE_FLASH_ATTR supla_esp_board_cfg_html_template(
       "</select><label>Thermometer type:</label></i>"
 	  "<i><select name=\"upd\"><option value=\"0\" "
       "%s>NO<option value=\"1\" %s>YES</select><label>Firmware "
-      "update</label></i></div><button type=\"submit\">SAVE</button></form></div><br><br>";
+      "update</label></i></div><button type=\"submit\">SAVE</button><input "
+	  "type=\"hidden\" name=\"rbt\" value=\"2\" /></form></div><br><br></body></html>";
 
   int bufflen = strlen(supla_esp_devconn_laststate())
 				+strlen(dev_name)
