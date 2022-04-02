ced3ca5886b78f7f53c12c99416caec65cdd72ea
krissgpl <kris.sg@wp.pl>
2022-04-02 19:19:34 +0200

    test 1

--- src/include/board/k_gate_module_v3.h
+++ src/include/board/k_gate_module_v3.h
@@ -92,7 +92,7 @@
 											supla_log(LOG_DEBUG, "IP FIELD = %i", ipaddr_addr(SUPLA_ESP_SOFTVER)); };	\
 										if ( ChannelNumber == 0 ) {	\
 											state->Fields |= SUPLA_CHANNELSTATE_FIELD_LASTCONNECTIONRESETCAUSE;	\
-											state->LastConnectionResetCause = supla_board_gate; };	\
+											state->LastConnectionResetCause = &supla_board_gate; };	\
 
 void ICACHE_FLASH_ATTR supla_esp_board_send_channel_values_with_delay(void *srpc);
 		
