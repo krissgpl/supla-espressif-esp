76048cff7ece54a7bf6af46b37f62e8241d6f20b
Kris_work <krisu.sg@gmail.com>
2022-04-07 17:49:34 +0200

    poprawka definicji

--- src/include/board/k_sonoff_touch.c
+++ src/include/board/k_sonoff_touch.c
@@ -88,7 +88,7 @@ void supla_esp_baord_Port_OFF_cb(void *timer_arg) {
 void supla_esp_board_gpio_init(void) {
 		
 	supla_input_cfg[0].type = INPUT_TYPE_BTN_MONOSTABLE;
-	supla_input_cfg[0].gpio_id = B_BTN1_PORT;
+	supla_input_cfg[0].gpio_id = B_CFG_PORT;
 	supla_input_cfg[0].flags = INPUT_FLAG_PULLUP | INPUT_FLAG_CFG_BTN | INPUT_FLAG_TRIGGER_ON_PRESS;
 	supla_input_cfg[0].action_trigger_cap = SUPLA_ACTION_CAP_HOLD |
 											SUPLA_ACTION_CAP_SHORT_PRESS_x1 |
