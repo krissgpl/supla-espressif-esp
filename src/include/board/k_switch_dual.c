ed6d5508c46037ed8ddb340867776e5c7ea0ad8a
krissgpl <kris.sg@wp.pl>
2022-11-05 16:09:58 +0100

    "!" remove from html

--- src/include/board/k_switch_dual.c
+++ src/include/board/k_switch_dual.c
@@ -390,7 +390,7 @@ char *ICACHE_FLASH_ATTR supla_esp_board_cfg_html_template(
 	  "<i><select name=\"upd\"><option value=\"0\" "
       "%s>NO<option value=\"1\" %s>YES</select><label>Firmware "
       "update</label></i></div><button type=\"submit\">SAVE</button><input "
-	  "type=\"hidden\" name=\"rbt\" value=\"2\" /></form></div><br><br></body></html>!";
+	  "type=\"hidden\" name=\"rbt\" value=\"2\" /></form></div><br><br></body></html>";
 
   int bufflen = strlen(supla_esp_devconn_laststate())
 				+strlen(dev_name)
