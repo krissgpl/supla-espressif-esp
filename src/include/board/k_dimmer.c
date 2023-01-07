e0198ab8986c64aca6b7abc72bd15afe053cdefd
krissgpl <kris.sg@wp.pl>
2023-01-07 12:36:27 +0100

    after interrupt brightness should resume

--- src/include/board/k_dimmer.c
+++ src/include/board/k_dimmer.c
@@ -152,6 +152,7 @@ void dimmer_timer_OFF_cb(void *timer_arg) {
 	if ( gpio__input_get(B_SENSOR_PORT1) == 0 && supla_esp_gpio_output_is_hi(B_SENSOR_BLOCK1) == 1 ) {
 		supla_log(LOG_DEBUG, "Set dimmer 1 przerwanie");
 		Wlacznik1 = 1;
+		Licznik = Licznik2;
 		os_timer_disarm(&dimmer_timer);
 		os_timer_setfn(&dimmer_timer, (os_timer_func_t *)dimmer_timer_ON_cb, NULL);
 		os_timer_arm(&dimmer_timer, 10, 1);  };
@@ -159,6 +160,7 @@ void dimmer_timer_OFF_cb(void *timer_arg) {
 	if ( gpio__input_get(B_SENSOR_PORT2) == 0 && supla_esp_gpio_output_is_hi(B_SENSOR_BLOCK2) == 1 ) {
 		supla_log(LOG_DEBUG, "Set dimmer 2 przerwanie");
 		Wlacznik2 = 1;
+		Licznik = Licznik2;
 		os_timer_disarm(&dimmer_timer);
 		os_timer_setfn(&dimmer_timer, (os_timer_func_t *)dimmer_timer_ON_cb, NULL);
 		os_timer_arm(&dimmer_timer, 10, 1);  };
