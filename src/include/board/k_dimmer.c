6306ef7d69b657cdcfe01b5f25339fe63081d6ff
krissgpl <kris.sg@wp.pl>
2023-01-09 10:00:21 +0100

    too often read from flash

--- src/include/board/k_dimmer.c
+++ src/include/board/k_dimmer.c
@@ -145,11 +145,12 @@ void dimmer_timer_ON_cb(void *timer_arg) {
 	supla_log(LOG_DEBUG, "brightness_log : %i", brightness_log[Licznik]);
 	board_esp_pwm_set_percent_duty(brightness_log[Licznik], 0);
 	
-	if ( supla_esp_gpio_output_is_hi(B_HARMONOGRAM) == 1 ) { Jasnosc = 199;
-	} else { Jasnosc = supla_esp_state.brightness[0] * 2; };
+	//if ( supla_esp_gpio_output_is_hi(B_HARMONOGRAM) == 1 ) { Jasnosc = 199;
+	//} else { Jasnosc = supla_esp_state.brightness[0] * 2; };
 	
 	 if ( brightness_log[Licznik] == Jasnosc ) { 
 	 supla_log(LOG_DEBUG, "Dimmer Timer ON stop");
+	 supla_log(LOG_DEBUG, "stop - Licznik : %i", Licznik);
 	 //Licznik2 = Licznik+1;
 	 //supla_log(LOG_DEBUG, "Licznik2 : %i", Licznik2);
 	 os_timer_disarm(&dimmer_timer); }
@@ -164,7 +165,7 @@ void dimmer_timer_OFF_cb(void *timer_arg) {
 	//supla_log(LOG_DEBUG, "Licznik2 : %i", Licznik2);
 	supla_log(LOG_DEBUG, "Licznik : %i", Licznik);
 	supla_log(LOG_DEBUG, "brightness_log : %i", brightness_log[Licznik]);
-	board_esp_pwm_set_percent_duty(brightness_log[Licznik-1], 0);
+	board_esp_pwm_set_percent_duty(brightness_log[Licznik], 0);
 	
 	if ( gpio__input_get(B_SENSOR_PORT1) == 0 && supla_esp_gpio_output_is_hi(B_SENSOR_BLOCK1) == 1 ) {
 		supla_log(LOG_DEBUG, "Set dimmer 1 przerwanie");
@@ -215,8 +216,12 @@ void supla_dimmer_smooth(int in1, int in2) {
 	
 	if ( supla_esp_gpio_output_is_hi(B_SWITCH) == 1 && supla_esp_cfg.FirmwareUpdate == 0 ) {
 		
+		if ( supla_esp_gpio_output_is_hi(B_HARMONOGRAM) == 1 ) { Jasnosc = 199;
+		} else { Jasnosc = supla_esp_state.brightness[0] * 2; };
+		
 		Czas = supla_esp_state.brightness[1] * 1000;
 		supla_log(LOG_DEBUG, "Czas = %i ms", Czas);
+		supla_log(LOG_DEBUG, "Jasnosc (2x) = %i", Jasnosc);
 		supla_log(LOG_DEBUG, "Wlacznik1 = %i", Wlacznik1);
 		supla_log(LOG_DEBUG, "Wlacznik2 = %i", Wlacznik2);
 		
