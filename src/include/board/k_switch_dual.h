d513e7f0827395b04335ee072093333d89aeb788
krissgpl <kris.sg@wp.pl>
2023-01-18 14:34:11 +0100

    add macro for input notif

--- src/include/board/k_switch_dual.h
+++ src/include/board/k_switch_dual.h
@@ -34,8 +34,6 @@
 
 #define TEMP_SELECT
 
-#define BOARD_INPUT_STATE_CHANGE_NOTIF
-
 //#define BTN_MODE_PUBLISH_AT
 
 #define AP_SSID "SWITCH_DUAL"
@@ -110,7 +108,9 @@
 							
 #define BOARD_SEND_AT supla_send_at(input_cfg->gpio_id, action);
 
-void supla_esp_board_input_state_change(supla_input_cfg_t *input_cfg);
+#define BOARD_INPUT_STATE_CHANGE_SEND supla_esp_board_input_state_change(input_cfg->gpio_id, new_state);
+
+void supla_esp_board_input_state_change(uint8 gpio, int state);
 
 void supla_board_input(void);
 
--- src/user/supla_esp_input.c
+++ src/user/supla_esp_input.c
@@ -132,6 +132,10 @@ void GPIO_ICACHE_FLASH supla_esp_input_notify_state_change(
   supla_esp_board_input_state_change(input_cfg);
 #endif
 
+#ifdef BOARD_INPUT_STATE_CHANGE_SEND
+  BOARD_INPUT_STATE_CHANGE_SEND
+#endif	// moje
+
   if (supla_esp_input_is_advanced_mode_enabled(input_cfg)) {
     os_timer_setfn(&input_cfg->timer, supla_esp_input_advanced_timer_cb,
         input_cfg);
