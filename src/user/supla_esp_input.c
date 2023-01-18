c8f371ca2c6b7bfd56339771f1b176257af4eda2
krissgpl <kris.sg@wp.pl>
2023-01-18 11:06:25 +0100

    add log

--- src/user/supla_esp_input.c
+++ src/user/supla_esp_input.c
@@ -472,10 +472,10 @@ void GPIO_ICACHE_FLASH supla_esp_input_advanced_state_change_handling(
           if (input_cfg->type == INPUT_TYPE_MOTION_SENSOR) {
             supla_esp_gpio_on_input_inactive(input_cfg);
           }
-		  if (input_cfg->type == INPUT_TYPE_BTN_MONOSTABLE) {		// moje
+		/*  if (input_cfg->type == INPUT_TYPE_BTN_MONOSTABLE) {		// moje
             supla_esp_gpio_on_input_inactive(input_cfg);
 			supla_log(LOG_DEBUG, "supla_esp_input_advanced_state_change_handling - supla_esp_gpio_on_input_inactive");	// moje
-          }
+          } */
         }
       }
 
@@ -552,6 +552,12 @@ void GPIO_ICACHE_FLASH supla_esp_input_advanced_timer_cb(void *timer_arg) {
         }
       }
   }
+  
+  if (input_cfg->last_state == INPUT_STATE_INACTIVE || 
+	  input_cfg->type == INPUT_TYPE_BTN_MONOSTABLE) {		// moje
+            //supla_esp_gpio_on_input_inactive(input_cfg);
+			supla_log(LOG_DEBUG, "supla_esp_input_advanced_timer_cb - supla_esp_gpio_on_input_inactive");	// moje
+  
 }
 
 // Send action triger notification to server
