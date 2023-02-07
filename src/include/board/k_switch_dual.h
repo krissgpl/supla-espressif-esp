6b7be269c4fd5211c361169b651480b76630e03d
krissgpl <kris.sg@wp.pl>
2023-02-07 17:35:50 +0100

    rename void

--- src/include/board/k_switch_dual.h
+++ src/include/board/k_switch_dual.h
@@ -109,7 +109,7 @@
 							
 #define BOARD_SEND_AT supla_send_at(input_cfg->gpio_id, action);
 
-#define BOARD_INPUT_STATE_CHANGE_SEND supla_esp_board_input_state_change(input_cfg->gpio_id, new_state);
+#define BOARD_INPUT_STATE_CHANGE_SEND supla_esp_board_input_state_change2(input_cfg->gpio_id, new_state);
 
 void supla_esp_board_input_state_change2(uint8 gpio, int state);
 
