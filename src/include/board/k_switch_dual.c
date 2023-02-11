92014218009ec5b33b56303dc4774b88315a7564
krissgpl <kris.sg@wp.pl>
2023-02-11 19:58:21 +0100

    not needed

--- src/include/board/k_switch_dual.c
+++ src/include/board/k_switch_dual.c
@@ -89,7 +89,7 @@ void supla_esp_baord_value_timer2_cb(void *timer_arg) {
 	//supla_log(LOG_DEBUG, "supla_esp_state.Relay[1] : %d", supla_esp_state.Relay[1]);
 	
 	if ( supla_esp_state.Relay[0] == 1 || supla_esp_state.Relay[1] == 1 ) {
-		if ( supla_esp_state.Relay[3] == 0 && supla_esp_state.Relay[4] == 0 && supla_esp_state.Relay[5] == 0 ) {
+		if ( supla_esp_state.Relay[3] == 0 && supla_esp_state.Relay[4] == 0 ) {
 			if( supla_esp_cfg.ThermometerType == 1 || supla_esp_cfg.ThermometerType == 2) {
 				supla_esp_devconn_send_action_trigger(7, SUPLA_ACTION_CAP_SHORT_PRESS_x5);
 			} else { supla_esp_devconn_send_action_trigger(6, SUPLA_ACTION_CAP_SHORT_PRESS_x5); }
