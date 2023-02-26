48cac06c3bfce8da0fc22013dce95821de07a89c
krissgpl <kris.sg@wp.pl>
2023-02-26 12:54:43 +0100

    add if

--- src/include/board/k_rs_module_v2.c
+++ src/include/board/k_rs_module_v2.c
@@ -379,8 +379,11 @@ supla_esp_board_is_rs_in_move(supla_roller_shutter_cfg_t *rs_cfg);
 void ICACHE_FLASH_ATTR supla_esp_board_rs_motor_move( bool move ) {
 	
 	supla_roller_shutter_cfg_t *rs_cfg;
-	supla_esp_board_is_rs_in_move(rs_cfg) = move;
-
+	
+	if (move)
+		supla_esp_board_is_rs_in_move(rs_cfg) = true;
+	else
+		supla_esp_board_is_rs_in_move(rs_cfg) = false;
 }
 
 void GPIO_ICACHE_FLASH supla_block_channel(void) {
