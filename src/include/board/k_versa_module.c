7cc11b5e4856e35811d540d333dfa1ef1ffb3a51
krissgpl <kris.sg@wp.pl>
2024-03-13 20:14:15 +0100

    add log

--- src/include/board/k_versa_module.c
+++ src/include/board/k_versa_module.c
@@ -433,7 +433,7 @@ supla_esp_board_gpio_on_input_inactive(void* _input_cfg) {
 
 	supla_input_cfg_t* input_cfg = (supla_input_cfg_t*)_input_cfg;
 
-	supla_log(LOG_DEBUG, "board inactive");
+	supla_log(LOG_DEBUG, "board inactive, channel=%i",input_cfg->channel);
 
 	if (input_cfg->type == INPUT_TYPE_SENSOR && input_cfg->channel != 255) supla_esp_channel_value_changed(input_cfg->channel, 1);
 	
