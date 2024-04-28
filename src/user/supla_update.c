6b89c5d28540543dd970f54e35b1cc2ab6f5e654
krissgpl <kris.sg@wp.pl>
2024-04-28 08:54:41 +0200

    add log for debug

--- src/user/supla_update.c
+++ src/user/supla_update.c
@@ -474,6 +474,7 @@ supla_esp_update_connect_cb(void *arg) {
 			     update->url->url.host);
 
 	/*int s =*/ espconn_sent(&update->conn, (unsigned char*)request, strlen(request));
+	int s = espconn_sent(&update->conn, (unsigned char*)request, strlen(request));
 	supla_log(LOG_DEBUG, "espconn_sent %i, %i", s, strlen(request));
 }
 
